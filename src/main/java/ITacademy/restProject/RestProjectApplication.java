691562aec7b73d2e266a2215dd5749e986d4f468
cotoflux <guaupelu@hotmail.com>
2020-06-27 16:23:50 +0200

    Contestat punt2

--- src/main/java/ITacademy/restProject/RestProjectApplication.java
+++ src/main/java/ITacademy/restProject/RestProjectApplication.java
@@ -2,12 +2,20 @@ package ITacademy.restProject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class RestProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestProjectApplication.class, args);
 	}
-
+	
+	@GetMapping("/hello")
+	public String hello(@RequestParam(value = "name", defaultValue = "WORLD") String name) {
+	return String.format("HELLO %s!", name);
+	}
 }
